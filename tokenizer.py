f69e4fa860d58e7d11e6a7d7a49da5dc6d300745
Mariana <im.marianaas@gmail.com>
2023-08-29 20:53:40 +0200

    find imports, instantiated objects, class name and package

--- tokenizer.py
+++ tokenizer.py
@@ -6,6 +6,9 @@ path = "./course-02242-examples/src/dependencies/java/dtu/deps/simple/"
 # { principal class : [ immediate dependencies ] }
 dict = {}
 
+def strip_elems( l ):
+    return [ elem.strip() for elem in l]
+
 for file in os.listdir( path ):
     with open(path + file) as f:
 
@@ -18,23 +21,23 @@ for file in os.listdir( path ):
         # print("no commentt" + no_comment)
          
 
-        package = re.findall(r"(?<=package ).*(?=;)", no_comment )
-        class_name = re.findall(r"(?<=class ).*(?= )", no_comment )
+        package = strip_elems( re.findall(r"(?<=package\s)[\s\S]+?(?=;)", no_comment ) )
+        class_name = strip_elems( re.findall(r"(?<=class\s)[\s\S]+?(?=\{)", no_comment ))
 
-        direct_imports = re.findall(r"(?<=import ).*(?=;)", no_comment )
 
-        objects_created = re.findall(r"(?<=new ).*(?=\()", no_comment )
+        direct_imports = strip_elems( re.findall(r"(?<=import\s)[\s\S]+?(?=;)", no_comment ) )
+        new_objects = strip_elems( re.findall(r"(?<=new\s)[\s\S]+?(?=\()", no_comment ) )
 
         print("filename:", file,
               "\t| class name: ", package + class_name, 
               "\t| direct imports: ", direct_imports,
-              "\t| objects created: ", objects_created )
+              "\t| objects created: ", new_objects )
         
 
         main_class = package[0] + "." if package != [] else ""
         main_class += class_name[0]
 
-        dict[ main_class ] = direct_imports + objects_created
+        dict[ main_class ] = direct_imports + new_objects
 
 
 print()
