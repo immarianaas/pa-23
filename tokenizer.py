5bff3229caae78ce80ff915d7b8f0185db6d2cfe
Mariana <im.marianaas@gmail.com>
2023-08-30 20:52:38 +0200

    join classes with package

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,10 @@
 import os
 import re
-path = "./course-02242-examples/src/dependencies/java/dtu/deps/simple/"
+import pprint 
+pp = pprint.PrettyPrinter(indent=4)
+
+
+path = "./course-02242-examples/src/dependencies/java/dtu/deps/tricky/"
 
 
 # { principal class : [ immediate dependencies ] }
@@ -40,7 +44,39 @@ for file in os.listdir( path ):
         dict[ main_class ] = direct_imports + new_objects
 
 
+
+def complete_dict():
+    new_dict = {}
+    for k, v in dict.items():
+        list_complete_classes = list( filter( lambda elem: len( elem.split('.') ) > 1, v ) ) + [k]
+        complete_classes = set( [ elem.split('.')[-1] for elem in list_complete_classes ] )
+        short_classes = set( filter( lambda elem: len( elem.split('.') ) == 1, v ) )
+
+        '''
+        print("..")
+        print(complete_classes)
+        print(short_classes)
+        print( "intersection:", complete_classes & short_classes )
+        '''
+        missing_package = short_classes - complete_classes
+        # print( "in the same package (missing package):", missing_package )
+
+        package = k.rsplit('.', 1 )[0]
+        #list_complete_classes += [ package + "." + sc for sc in missing_package ]
+        list_complete_classes += [ sc for sc in missing_package ]
+
+        new_dict[k] = [elem for elem in list_complete_classes if elem != k ]
+
+    return new_dict
+
+
 print()
 print( dict )
         
+
+new_dict= complete_dict()
+
+print("--")
+
+pp.pprint(new_dict)
 
