32aca259f5b14638eebe832b1b0b599bee6aff7a
Mariana <im.marianaas@gmail.com>
2023-09-04 17:20:51 +0200

    starting to use tree sitter

--- assignment-2/ClassDiagram.py
+++ assignment-2/ClassDiagram.py
@@ -1,10 +1,52 @@
 from tree_sitter import Language, Parser
-FILE = "./languages.so" # the ./ is important
-Language.build_library(FILE, ["tree-sitter-java"])
+FILE = "./../java.so" # the ./ is important
+Language.build_library(FILE, ["./../tree-sitter-java"])
 JAVA_LANGUAGE = Language(FILE, "java")
 parser = Parser()
 parser.set_language(JAVA_LANGUAGE)
-with open("Test.java", "rb") as f:
-tree = parser.parse(f.read())
+
+example_path = "../example-dependency-graphs/src/main/java/dtu/compute/simple/Example.java"
+
+with open(example_path, "rb") as f:
+    tree = parser.parse(f.read())
 # the tree is now ready for analysing
 print(tree.root_node.sexp())
+
+
+
+class SyntaxFold:
+
+    def visit( self, node ):
+        results = [ self.visit( n ) for n in node.children ]
+
+        if hasattr( self, node.type ):
+            return getattr(self, node.type)(node, results)
+        
+        return self.default(node, results)
+    
+    # def default(self, node, results): # TODO: we prob dont need these 'results'
+    #     print("DEFAULT:", node )
+
+    
+class Printer( SyntaxFold ):
+    def default(self,node, results):
+        print(node)
+        print(results)
+
+class TypeIdentifiers( SyntaxFold ):
+    def default(self, node, results):
+        return set().union(*results)
+    
+    def type_identifier(self, node, results):
+        return { b"a: " + node.text }
+    
+    def scoped_identifier( self, node, results):
+        return { b"b: " + node.text }
+    
+    def package_declaration( self, node, results ):
+        return { b"c: " + node.text }
+
+# Printer().visit( tree.root_node )
+
+print()
+print( TypeIdentifiers().visit( tree.root_node ) )
