d12d12004f839494524ef1ac62effd623329fe06
KajsaAviaja <kajsaaviaja@gmail.com>
2023-09-16 12:20:38 +0200

    wip

--- assignment-3/Analyser.py
+++ assignment-3/Analyser.py
@@ -6,29 +6,20 @@ import json
 dict = {}
 
 
-def createDictEntry(className):
-    dict[className] = {
-        "relationships": {
-            "Dependency": [],
-            "Association": [],
-            "Aggregation": [],
-            "Composition": [],
-            "Realization": [],
-            "Inheritance": [],
-        },
-        "fields": {
-            "Public": {"name": "", "type": "", "val": ""},
-            "Private": {"name": "", "type": "", "val": ""},
-            "Protected": {"name": "", "type": "", "val": ""},
-            "Package": {"name": "", "type": "", "val": ""},
-        },
-        "functions": {
-            "Public": {"name": "", "arg": {}, "return": ""},
-            "Private": {"name": "", "arg": {}, "return": ""},
-            "Protected": {"name": "", "arg": {}, "return": ""},
-            "Package": {"name": "", "arg": {}, "return": ""},
-        },
-    }
+def createDictEntry(className: str):
+    if className not in dict.keys():
+        dict[className] = {
+            "relationships": {
+                "Dependency": [],
+                "Association": [],
+                "Aggregation": [],
+                "Composition": [],
+                "Realization": [],
+                "Inheritance": [],
+            },
+            "fields": [],
+            "functions": [],
+        }
 
 
 def JsonToDictEntries(data: json):
@@ -36,10 +27,25 @@ def JsonToDictEntries(data: json):
 
     createDictEntry(classname)
 
-    print(data)
+    GetInterfaces(data, classname)
+    fields = data["fields"]
+    for f in fields:
+        dict[classname]["fields"].append(f["name"])
 
 
-def JsonDirectoryToDict(json_directory):
+def GetInterfaces(data: json, classname: str):
+    interfaces = data["interfaces"]
+    for i in interfaces:
+        i_name = i["name"]
+        dict[classname]["relationships"]["Realization"].append(i_name)
+        createDictEntry(i_name)
+        for arg in i["args"]:
+            if arg["type"]["kind"] == "class":
+                createDictEntry(i_name)
+                dict[i_name]["relationships"]["Dependency"].append(arg["type"]["name"])
+
+
+def JsonDirectoryToDict(json_directory: str):
     for file in os.listdir(json_directory):
         f = open(json_directory + "\\" + file)
         data: json = json.load(f)
