03043ea1093f34bd94abd897a0f4a48bbb79c71c
KajsaAviaja <kajsaaviaja@gmail.com>
2023-09-16 11:59:15 +0200

    moves dict out of fuction to avoid duplicates/ovverwrites

--- assignment-3/Analyser.py
+++ assignment-3/Analyser.py
@@ -3,52 +3,49 @@ from CreateGraph import CreateGraph
 import json
 
 
+dict = {}
+
+
 def createDictEntry(className):
-    return {
-        className: {
-            "relationships": {
-                "Dependency": [],
-                "Association": [],
-                "Aggregation": [],
-                "Composition": [],
-                "Realization": [],
-                "Inheritance": [],
-            },
-            "fields": {
-                "Public": {"name": "", "type": "", "val": ""},
-                "Private": {"name": "", "type": "", "val": ""},
-                "Protected": {"name": "", "type": "", "val": ""},
-                "Package": {"name": "", "type": "", "val": ""},
-            },
-            "functions": {
-                "Public": {"name": "", "arg": {}, "return": ""},
-                "Private": {"name": "", "arg": {}, "return": ""},
-                "Protected": {"name": "", "arg": {}, "return": ""},
-                "Package": {"name": "", "arg": {}, "return": ""},
-            },
-        }
+    dict[className] = {
+        "relationships": {
+            "Dependency": [],
+            "Association": [],
+            "Aggregation": [],
+            "Composition": [],
+            "Realization": [],
+            "Inheritance": [],
+        },
+        "fields": {
+            "Public": {"name": "", "type": "", "val": ""},
+            "Private": {"name": "", "type": "", "val": ""},
+            "Protected": {"name": "", "type": "", "val": ""},
+            "Package": {"name": "", "type": "", "val": ""},
+        },
+        "functions": {
+            "Public": {"name": "", "arg": {}, "return": ""},
+            "Private": {"name": "", "arg": {}, "return": ""},
+            "Protected": {"name": "", "arg": {}, "return": ""},
+            "Package": {"name": "", "arg": {}, "return": ""},
+        },
     }
 
 
 def JsonToDictEntries(data: json):
     classname = data["name"]
 
-    dict = createDictEntry(classname)
+    createDictEntry(classname)
 
     print(data)
 
-    return dict
-
 
 def JsonDirectoryToDict(json_directory):
-    dict = {}
     for file in os.listdir(json_directory):
         f = open(json_directory + "\\" + file)
         data: json = json.load(f)
         f.close()
-        dict.update(JsonToDictEntries(data))
-    return dict
+        JsonToDictEntries(data)
 
 
-dict = JsonDirectoryToDict("assignment-3\Json bytecode")
+JsonDirectoryToDict("assignment-3\Json bytecode")
 print(json.dumps(dict, indent=4))
