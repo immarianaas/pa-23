6aa7fe56f158cb16912e0560d126520f6d9223f6
kajsa <kajsaaviaja@gmail.com>
2023-10-02 13:27:35 +0200

    some static analysis

--- assignment-5/exeptionTest.py
+++ assignment-5/exeptionTest.py
@@ -29,3 +29,67 @@ print(staticInterpreter.interpretBytecode([
             "type": "int"
           }
         ]))
+
+print(staticInterpreter.interpretBytecode([
+          {
+            "offset": 0,
+            "opr": "push",
+            "value": {
+              "type": "integer",
+              "value": 3
+            }
+          },
+          {
+            "offset": 1,
+            "opr": "store",
+            "type": "int",
+            "index": 1
+          },
+          {
+            "offset": 2,
+            "opr": "load",
+            "type": "int",
+            "index": 1
+          },
+          {
+            "offset": 3,
+            "opr": "load",
+            "type": "int",
+            "index": 1
+          },
+          {
+            "offset": 4,
+            "opr": "binary",
+            "type": "int",
+            "operant": "sub"
+          },
+          {
+            "offset": 5,
+            "opr": "store",
+            "type": "int",
+            "index": 1
+          },
+          {
+            "offset": 6,
+            "opr": "load",
+            "type": "int",
+            "index": 0
+          },
+          {
+            "offset": 7,
+            "opr": "load",
+            "type": "int",
+            "index": 1
+          },
+          {
+            "offset": 8,
+            "opr": "binary",
+            "type": "int",
+            "operant": "div"
+          },
+          {
+            "offset": 9,
+            "opr": "return",
+            "type": "int"
+          }
+        ]))
--- assignment-5/staticInterpreter.py
+++ assignment-5/staticInterpreter.py
@@ -4,31 +4,51 @@ import glob
 import subprocess
 from collections import defaultdict
 
+def interpretMethod(filepath, methodname):
+    
+    f = open("course-02242-examples/decompiled/eu/bogoe/dtu/exceptional/Arithmetics.json")
+   
+    obj = json.load(f)
+    f.close()
+    methods = obj["methods"]
+    output = [i for i in methods if i["name"] == "alwaysThrows1"]
+    m = output[0]
+    return interpretBytecode(m["code"]["bytecode"])
+   
+    
+
+    
+
 
 def interpretBytecode(byteArray, index=0, stack=[], memory={}):
     byteObj = byteArray[index]
-    # print(byteObj, '\n')
     match byteObj["opr"]:   
         case "push":
-            stack.append(byteObj["value"]) 
+            stack.append((byteObj["value"]["type"], [0])) 
         case "binary":
-            a = stack.pop()
-            b = stack.pop()
-            print(a)
+            (a_type, a_ranges) = stack.pop()
+            (b_type, b_ranges) = stack.pop()
             match byteObj["operant"]:
                 case "div":
-                    if a["value"] == 0:
-                        return "Trows 1"
+                    if a_type != "integer":
+                        return "UnsupportedOperationException"
+                    for i in a_ranges:
+                        if i == 0:
+                            return "ArithmeticException"
+                        if isinstance(i, tuple) & (i[0] <= 0 <= i[1]):
+                            return "ArithmeticException"
+                case _ : 
+                    print(byteObj["operant"], "not implemented")
+                    return
 
                     
         case _:
             print(byteObj["opr"] + " not implemented")
             return
 
-    # print(byteArray)
-    if (len(byteArray) > index):
-        return interpretBytecode(byteArray, index+1, stack, memory)
-    else:
-        return "something"
+    
+    return interpretBytecode(byteArray, index+1, stack, memory)
 
 
+
+print(interpretMethod("course-02242-examples/decompiled/eu/bogoe/dtu/exceptional/Arithmetics.json", "alwaysThrows2"))
