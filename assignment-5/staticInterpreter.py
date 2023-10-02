877becd778939d655b5f1fc037bb545c3117cb17
kajsa <kajsaaviaja@gmail.com>
2023-10-02 15:39:57 +0200

    more static analysis

--- assignment-5/staticInterpreter.py
+++ assignment-5/staticInterpreter.py
@@ -11,7 +11,7 @@ def interpretMethod(filepath, methodname):
     obj = json.load(f)
     f.close()
     methods = obj["methods"]
-    output = [i for i in methods if i["name"] == "alwaysThrows1"]
+    output = [i for i in methods if i["name"] == "alwaysThrows2"]
     m = output[0]
     return interpretBytecode(m["code"]["bytecode"])
    
@@ -20,26 +20,30 @@ def interpretMethod(filepath, methodname):
     
 
 
-def interpretBytecode(byteArray, index=0, stack=[], memory={}):
+def interpretBytecode(byteArray, index=0, stack=[], memory={}, exeptions = []):
     byteObj = byteArray[index]
     match byteObj["opr"]:   
         case "push":
-            stack.append((byteObj["value"]["type"], [0])) 
+            stack.append((byteObj["value"]["type"], [(byteObj["value"]["value"],byteObj["value"]["value"])])) 
         case "binary":
             (a_type, a_ranges) = stack.pop()
             (b_type, b_ranges) = stack.pop()
             match byteObj["operant"]:
                 case "div":
                     if a_type != "integer":
-                        return "UnsupportedOperationException"
+                        exeptions.append((index, "UnsupportedOperationException"))
                     for i in a_ranges:
-                        if i == 0:
-                            return "ArithmeticException"
-                        if isinstance(i, tuple) & (i[0] <= 0 <= i[1]):
-                            return "ArithmeticException"
+                        if (i[0] <= 0 <= i[1]):
+                            exeptions.append((index, "ArithmeticException"))
+
                 case _ : 
-                    print(byteObj["operant"], "not implemented")
+                    print(byteObj["operant"], " binary operation not implemented")
                     return
+        case "store":
+            memory[byteObj["index"]] = stack.pop()
+        case "load":
+            stack.append(memory[byteObj["index"]])
+            
 
                     
         case _:
@@ -47,7 +51,7 @@ def interpretBytecode(byteArray, index=0, stack=[], memory={}):
             return
 
     
-    return interpretBytecode(byteArray, index+1, stack, memory)
+    return interpretBytecode(byteArray, index+1, stack, memory, exeptions)
 
 
 
