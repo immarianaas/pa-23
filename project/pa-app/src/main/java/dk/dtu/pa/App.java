d34828e49e4a7a263614e56fc9da56facc0742eb
Mariana <im.marianaas@gmail.com>
2023-10-23 15:23:55 +0200

    add entry point and functions

--- project/pa-app/src/main/java/dk/dtu/pa/App.java
+++ project/pa-app/src/main/java/dk/dtu/pa/App.java
@@ -1,18 +1,30 @@
 package dk.dtu.pa;
 
 import dk.dtu.pa.geometry.Square;
+import dk.dtu.pa.utils.EntryPoint;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
         Square sq = new Square(5);
 
-        System.out.println( "Hello World! Square has perimeter: " + sq.perimeter() );
+        System.out.println("Hello World! Square has perimeter: " + sq.perimeter());
+    }
+
+    @EntryPoint
+    public int helloWorld() {
+        int i = 2;
+        int x = i + 1;
+        return i + x;
+    }
+
+    @EntryPoint
+    public double myFirstFunction() {
+        Square sq = new Square(2);
+        return sq.perimeter();
     }
 }
---
+++ project/pa-app/src/main/java/dk/dtu/pa/utils/EntryPoint.java
@@ -0,0 +1,10 @@
+package dk.dtu.pa.utils;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+public @interface EntryPoint {
+    
+}
+
