802ce1ff40180bf52ffeeeb33218de4036d897ef
kajsa <kajsaaviaja@gmail.com>
2023-11-06 14:40:42 +0100

    more demo functions

---
+++ demo/src/main/java/com/example/Vehicle/Bike.java
@@ -0,0 +1,8 @@
+package com.example.Vehicle;
+
+public class Bike extends Vehicle {
+    @Override
+    public void drive() {
+        // You'll have to do the work yourself now!
+    }
+}
---
+++ demo/src/main/java/com/example/Vehicle/Cabriolet.java
@@ -0,0 +1,5 @@
+package com.example.Vehicle;
+
+public class Cabriolet extends Car {
+    // This drives exactly like a normal car
+}
