faf0a19d03e1225788f2f603e31388d1ce2260b1
KajsaAviaja <kajsaaviaja@gmail.com>
2023-11-07 21:30:05 +0100

    wip

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -7,10 +7,5 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Test.test1();
-        Test.test2();
-        Test.test3();
-        System.out.println(Test.test6());
-        System.out.println(43 / 8);
     }
 }
