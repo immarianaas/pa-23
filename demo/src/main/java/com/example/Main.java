a37171bc00022b480962c41042dfa21c8b73d0d8
Mariana <im.marianaas@gmail.com>
2023-11-20 14:05:18 +0100

    dunno

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,10 +2,18 @@ package com.example;
 
 public class Main {
 
-    public Main() {
-
-    }
-
     public static void main(String[] args) {
+        Main m = new Main();
+        System.out.println(m.ret1also());
     }
+    
+    public int ret1() {
+        return 1;
+    }
+
+    public int ret1also() {
+        return ret1();
+    }
+
+    
 }
--- project/Interpreter/testDemo.py
+++ project/Interpreter/testDemo.py
@@ -1,20 +1,28 @@
 from Util import Heap, Operand, OperandStack, PrimitiveTypes, StackFrame
 from Interpreter import InterpretFunction
+import os
+
+this_path = os.path.dirname(os.path.abspath(__file__))
+dir = os.path.join(this_path, "..", "..", "demo", "decompiled", "classes")
+file = os.path.join("com", "example", "Test")
+
+# file = os.path.join(dir, "com", "example", "Test")
 
 # For window
-dir = "demo\decompiled\classes"
-file = "com\example\Test"
+#dir = "demo\decompiled\classes"
+#file = "com\example\Test"
 
 # For linux
-# dir = "demo/decompiled/classes"
-# file = "com/example/Test"
+#dir = "demo/decompiled/classes"
+#file = "com/example/Test"
 
-
+test1_set = set()
 InterpretFunction(
     dir=dir,
     file=file,
+    edges = test1_set,
     function="test1",
-    printDebug=True,
+    #printDebug=True,
 )
 
 assert (
@@ -22,7 +30,7 @@ assert (
         dir=dir,
         file=file,
         function="test1",
-        printDebug=True,
+        #printDebug=True,
     )[0].get_value()
     == 1
 )
@@ -69,12 +77,22 @@ assert (
     == 45
 )
 
+
+test6_set = set()
 assert (
     InterpretFunction(
         dir=dir,
         file=file,
+        edges= test6_set,
         function="test6",
         # printDebug=True,
     )[0].get_value()
     == 5
 )
+
+print("\n\n\n")
+print(test1_set)
+
+
+print("\n\n\n")
+print(test6_set)
